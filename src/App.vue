067d65ca2779551b655206bad2c409786df7d272
David Mordechai <david.mordechai79@gmail.com>
2023-02-15 22:29:59 +0200

    lesson 38

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
+<template>
+  <FilesBrowser />
+  <hr />
+  <Learning />
+  <hr />
+  <Watchers />
+  <hr />
+  <Article title="Article Title" :likes="50" :isPublished="true" id="my-article" />
+  <hr />
+  <ComponentA />
+  <hr />
+  <button @click="showPopup = true">Show popup</button>
+  <Popup v-show="showPopup" @close="closePopup" />
+  <hr>
+  <Input v-model="name" />
+</template>
+
 <script>
 import Article from "./components/Article.vue";
 import FilesBrowser from "./components/FilesBrowser.vue";
@@ -5,11 +22,13 @@ import Learning from "./components/Learning.vue";
 import Watchers from "./components/Watchers.vue";
 import ComponentA from "./components/ComponentA.vue";
 import Popup from "./components/Popup.vue";
+import Input from "./components/input.vue";
 export default {
-  components: { FilesBrowser, Learning, Watchers, Article, ComponentA, Popup },
+  components: { FilesBrowser, Learning, Watchers, Article, ComponentA, Popup, Input },
   data() {
     return {
       showPopup: false,
+      name: ''
     };
   },
   methods: {
@@ -21,28 +40,8 @@ export default {
 };
 </script>
 
-<template>
-  <FilesBrowser />
-  <hr />
-  <Learning />
-  <hr />
-  <Watchers />
-  <hr />
-  <Article
-    title="Article Title"
-    :likes="50"
-    :isPublished="true"
-    id="my-article"
-  />
-  <hr />
-  <ComponentA />
-  <hr />
-  <button @click="showPopup = true">Show popup</button>
-  <Popup v-show="showPopup" @close="closePopup" />
-</template>
-
 <style>
-body {
+#app {
   text-align: center !important;
 }
 </style>
